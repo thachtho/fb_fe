0110fa788b11df3497bcebbfd37a6323cba481e0
thobui14ig <buithanhtho14ig@gmail.com>
2024-06-04 23:22:36 +0700

    ok

--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -1,8 +1,8 @@
 const CONSTANT = {
   // URL: 'http://159.65.131.243/api',
   // URL_SOCKET: 'http://159.65.131.243'
-  URL: 'http://localhost:3000/api',
-  URL_SOCKET: 'http://localhost:3000'
+  URL: 'http://159.65.131.243/api',
+  URL_SOCKET: 'http://159.65.131.243'
 }
 
 export default CONSTANT
