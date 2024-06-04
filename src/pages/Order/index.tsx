aaf6fa3a140021c78aba953238b058fd8eb8a91e
thobui14ig <buithanhtho14ig@gmail.com>
2024-06-04 23:17:07 +0700

    ok

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -11,6 +11,7 @@ function OrderPage() {
   const { posts } = useOrder()
   useGetPost()
   useGetMessage()
+  console.log(111111, posts)
 
   return (
     <>
--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -1,8 +1,8 @@
 const CONSTANT = {
-  URL: 'https://hoctuvung.online/api',
-  URL_SOCKET: 'https://hoctuvung.online'
-  // URL: 'http://localhost:9000/api',
-  // URL_SOCKET: 'http://localhost:9000'
+  // URL: 'http://159.65.131.243/api',
+  // URL_SOCKET: 'http://159.65.131.243'
+  URL: 'http://localhost:3000/api',
+  URL_SOCKET: 'http://localhost:3000'
 }
 
 export default CONSTANT
