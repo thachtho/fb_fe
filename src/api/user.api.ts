17985b03d143272419848670bbb9a4a1385e4579
thobui14ig <buithanhtho14ig@gmail.com>
2024-06-22 15:42:24 +0700

    ok

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -2,7 +2,7 @@ import { IUser } from 'shared/interface'
 import http from './http'
 import { IMember } from 'pages/Admin/AcceptMember/state'
 
-export const createUser = (data: IUser) => http.post<IUser[]>('/users', data)
+export const createUser = (data: IUser) => http.post<IUser[]>('/auth/register', data)
 export const getUsers = () => http.get<IMember[]>('/users')
 export const getUserByPhone = (phone: string) => http.get<IMember[]>(`/users/phone/${phone}`)
 export const accepMember = (phone: string) => http.patch(`/users/access-by-phone/${phone}`)
