ff03442c214f4f13de5fea5b8f45fdc5374e4e41
thobui14ig <buithanhtho14ig@gmail.com>
2024-06-25 09:32:18 +0700

    ok

--- src/pages/Order/hooks/useGetMessage.tsx
+++ src/pages/Order/hooks/useGetMessage.tsx
@@ -69,39 +69,6 @@ const useGetMessage = () => {
     }
     socket?.on('postMessage', async (data: IPost) => {
       getDistanceV1(data)
-      // const newPosts = data.filter(item => item.content.length > 0)
-      // getAllDistanceAsync(newPosts)
-      // const posts = getPosts() || [];
-      // const dataCheck = posts.find((item) =>item.postId === data.postId);
-
-      // if (dataCheck) {
-      //   const currentPosition = getCurrentNavigator()
-        
-      //   const locationA = {   
-      //     latitude: currentPosition?.latitude,
-      //     longitude: currentPosition?.longitude
-      //   }
-      //   const startNavigator = data.startNavigator
-      //   const { lat, lng } = startNavigator || {}
-      //   const locationB = {   
-      //     latitude: lat,
-      //     longitude: lng 
-      //   }
-
-      //   if (locationA && locationB.latitude !== 0 && locationB.longitude.length !== 0) {
-      //     const distance = calculateDistance(locationA, locationB)
-      //     dataCheck.distance = parseFloat(distance.toFixed(1));
-      //   }
-
-      //   setPosts([...posts])
-
-      //   return;
-      // }
-
-      // posts.unshift({
-      //   ...data
-      // })
-      // setPosts(posts)
     })
   }, [socket])
 }
@@ -110,66 +77,31 @@ const useGetPost = () => {
   const { getCurrentNavigator } = useNavigator()
   const { setPosts } = useOrder()
 
-  const getAllDistanceAsync = async (posts: IPost[]) => {
+  const getAllDistanceAsyncV1 = async (posts: IPost[]) => {
     const currentPosition = getCurrentNavigator()
-
-    const locationA = {   
+    const locationMe = {   
       latitude: currentPosition?.latitude,
       longitude: currentPosition?.longitude
     }
 
-    for (const post of posts) {
-      try {
-        const startNavigator = post.startNavigator
-        const { lat, lng } = startNavigator || {}
-        const locationB = {   
-          latitude: lat,
-          longitude: lng 
+    for (const item of posts) {
+      if (locationMe && item?.location) {
+        try {
+          const distance = calculateDistance(locationMe, item?.location)
+          item.distance = distance
+        } catch (error) {
+          console.log('Distance error:::')
         }
-
-        if (locationA && (locationB?.latitude && locationB?.latitude !== 0) && (locationB?.longitude && locationB?.longitude !== 0)) {
-          const distance = calculateDistance(locationA, locationB)
-
-          post.distance = distance ? parseFloat(distance.toFixed(1)) : null;
-        }       
-      } catch (error) {
-        return null
       }
     }
 
     setPosts([...posts])
   }
 
-  const getAllDistanceAsyncV1 = async (posts: IPost[]) => {
-    const location = getCurrentNavigator()
-
-    const apis = posts.map((item) => {
-      const input = {
-        lat: location?.latitude,
-        long: location?.longitude,
-        address: getAddress(item.content) || null
-      }
-
-      return getDistance(input)
-    })
-
-    const responseDistance = await Promise.all(apis)
-    const newDataDistance = posts.map((item, i) => {
-      return {
-        ...item,
-        distance: responseDistance[i].length > 0 ? responseDistance[i] : null
-      }
-    })
-
-    setPosts([...newDataDistance])
-  }
-
   useEffect(() => {
     ;(async () => {
       const { data } = await getPost()
-      const newPosts = data.filter(item => item.content.length > 0)
-      setPosts(data);
-      await getAllDistanceAsyncV1(newPosts)
+      await getAllDistanceAsyncV1(data)
     })()
   }, [])
 }
